140d600a156a82b223958dbe0a469944b9341b1c
MikiJMA <julianvillegas191@gmail.com>
2026-04-10 17:12:06 -0600

    Actualizar TraductorGson.java

--- app/src/main/java/com/example/agendajjvr/persistencia/TraductorGson.java
+++ app/src/main/java/com/example/agendajjvr/persistencia/TraductorGson.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 
 public class TraductorGson {
 
-    // Agarra TODA la lista de contactos y la hace un solo texto JSON
+    
     public String empaquetarAgendaCompleta(ArrayList<Datos> listaContactos) {
         return new Gson().toJson(listaContactos);
     }
 
-    // Lee el súper bloque JSON y te devuelve la lista ya armada
+    
     public ArrayList<Datos> desempaquetarAgenda(String jsonGeneral) {
         if (jsonGeneral == null || jsonGeneral.isEmpty()) {
             return new ArrayList<>(); // Si está vacío, regresa una lista en blanco
         }
-        // Esta línea es magia de GSON para entender que es una Lista de objetos
+         
         Type tipoLista = new TypeToken<ArrayList<Datos>>(){}.getType();
         return new Gson().fromJson(jsonGeneral, tipoLista);
     }
